59146ca84a10f77cc4dbfd058c38548a7d13a159
Merih <merimemax@gmail.com>
2020-02-21 09:10:15 -0600

    Base layer for Controller method of AutoLoanController. Post/Get/Update/Delete paths successfully generated and producing a response when sent in Postman.

--- autoloan/src/main/java/com/example/banking/controller/AutoLoanController.java
+++ autoloan/src/main/java/com/example/banking/controller/AutoLoanController.java
@@ -1,4 +1,45 @@
 package com.example.banking.controller;
 
+import com.example.banking.service.AutoLoanService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/autoloan")
 public class AutoLoanController {
+
+    private AutoLoanService service;
+
+    public AutoLoanController(AutoLoanService service) {
+        this.service = service;
+    }
+
+    // Create
+    @PostMapping(value = "/createLoan", produces = "application/json")
+    public String createLoan(@RequestBody String information) {
+        return "CreateLoan successful.";
+    }
+
+    // Read
+    @GetMapping(value = "/getLoansByClientId/{clientId}", produces = "application/json")
+    public String getLoansByClientId(@PathVariable("clientId") int clientId) {
+        return "GetLoansByClientId successful " + clientId;
+    }
+
+    // Read All
+    @GetMapping(value = "/getAllLoans", produces = "application/json")
+    public String getAllLoans() {
+        return "GetAllLoans successful";
+    }
+
+    // Update
+    @PostMapping(value = "/updateLoan/{id}", produces = "application/json")
+    public String updateLoan(@PathVariable("id") int id) {
+        return "UpdateLoan successful " + id;
+    }
+
+    // Delete
+    @DeleteMapping(value = "/deleteLoan/{id}", produces = "application/json")
+    public String deleteLoan(@PathVariable("id") int id) {
+        return "DeleteLoan successful " + id;
+    }
 }
---
+++ autoloan/src/main/java/com/example/banking/service/AutoLoanService.java
@@ -0,0 +1,7 @@
+package com.example.banking.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutoLoanService {
+}
